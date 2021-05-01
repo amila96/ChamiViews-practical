6286e68ea1b8cda6115b17e8b71ad8f0942baf02
amila96 <64005937+amila96@users.noreply.github.com>
2021-05-01 11:55:54 +0530

    display data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import {TourPackagesComponent} from './tour-packages/tour-packages.component';
+import {DestinationsComponent} from './destinations/destinations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path :'home',component:HomeComponent},
+  {path : 'tourPackages',component:TourPackagesComponent},
+  {path :'destinations',component:DestinationsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TourPackagesComponent } from './tour-packages/tour-packages.component';
+import { DestinationsComponent } from './destinations/destinations.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TourPackagesComponent,
+    DestinationsComponent
   ],
   imports: [
     BrowserModule,
